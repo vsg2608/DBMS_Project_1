be6367cb4b3d6ad15555c76430fc4a1df0f023ef
Vishesh Goyal <vsg2608@gmail.com>
2019-02-23 00:13:30 +0530

    Search Working

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import psycopg2 as pg
-from flask import Flask, render_template, redirect, flash
-from forms import RegisterationForm, LoginForm
+from flask import Flask, render_template, redirect, flash,url_for
+from forms import RegisterationForm, LoginForm, searchForm
 import os
 import sys
 
@@ -36,24 +36,76 @@ def home():
 def about():
    return render_template('about.html',title='About')
 
-@app.route('/search')
+@app.route('/search', methods=['GET','POST'])
 def search():
    cur.execute("""SELECT restaurant_name,address, cuisines, averagecost_for_two, currency, aggregate_rating FROM rest LIMIT 10;""")
    restaurants= cur.fetchall()
-   print(restaurants[0][0])
-   return render_template('search.html',title='Find Restaurant',restaurants=restaurants)
+   form= searchForm()
+   if form.validate_on_submit():
+      print(form.locality.data)
+      print(form.rating.data)
+      query="SELECT distinct restaurant_name,address, cuisines, averagecost_for_two, currency, aggregate_rating from rest, city, cuisine,rest_cuisine WHERE rest.localityid=city.id and lower(city.city)=lower('"+str(form.city.data)+"') "
+      if(form.restaurantName.data!=""):
+         query+=" and lower(rest.restaurant_name) like lower('%" + form.restaurantName.data + "%') "
+      if(form.locality.data!=""):
+         query+=" and lower(city.locality) like lower('%" + form.locality.data + "%') "
+      if(form.rating.data!=""):
+         query+=" and rest.aggregate_rating>= "+str(form.rating.data)
+      cuisines="'japanese','korean'"
+      and_or=False
+      if(cuisines!=""):
+        query+= " and rest.restaurant_id= rest_cuisine.rest_id and cuisine.cuisine_code= rest_cuisine.cuisine_code and lower(cuisine.cuisine) in ("+cuisines+") "
+
+      sortbyRating= False
+      sortbyRestName= False
+      sortbyPrice= True
+      if(sortbyPrice):
+         query+= "ORDER BY rest.aggregate_rating desc"
+      print(query)
+      cur.execute(query+";")
+      restaurants= cur.fetchall()
+   return render_template('search.html',title='Find Restaurant',restaurants=restaurants, form=form)
    
 @app.route('/userRegister', methods=['GET','POST'])
 def userRegister():
    form= RegisterationForm()
    if form.validate_on_submit():
-      flash(f'Accound Created for { form.username.data}!', 'success')
-      return redirect(url_for('home'))
+      date="2018-12-30"
+      gender="M"
+      address="153 Phulkian Enclave, New Delhi"
+      locality="Hauz Khas"
+      name= form.name.data
+      email=form.email.data
+      password=form.password.data
+      cur.execute("Select email from users where email= '"+email+"';")
+      Email=cur.fetchall()
+      if(Email==[]):
+         flash(f'Account created for {form.name.data}!', 'success')
+         cur.execute("INSERT into users (password, name, email, dob, gender, address, locality) values " \
+                     "(%s, %s, %s, %s, %s,%s,%s);", (password,name, email, date, gender, address, locality))
+         conn.commit()
+         return redirect(url_for('home'))
+      else:
+         flash('Email already exists!!!!', 'danger')
+      
    return render_template('userRegister.html',title='User Register', form=form)
 
-@app.route('/userLogin')
+@app.route('/userLogin', methods=['GET','POST'])
 def userLogin():
    form= LoginForm()
+   print(form.email.data)
+   if form.validate_on_submit():
+      cur.execute("select password from users where email='"+str(form.email.data)+"';")
+      password=cur.fetchall()
+      if([]==password):
+         flash('Email not found. Please check your email', 'danger')
+      else:
+         password=password[0][0]
+         if form.password.data == password:
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+         else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
    return render_template('userlogin.html',title='User Login', form=form)
 
 if __name__ == '__main__':
