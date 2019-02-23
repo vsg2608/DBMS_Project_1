ef07e87261dbdfb67dae01b8c3dedc60ca3d776c
Vishesh Goyal <vsg2608@gmail.com>
2019-02-23 17:27:19 +0530

    User Login Working

--- restaurantApp/__init__.py
+++ restaurantApp/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= '45c0ae51e0e13c75a7c4ee3cc059388a'
+bcrypt= Bcrypt(app)
+login_manager= LoginManager(app)
 
 from restaurantApp import routes
--- restaurantApp/routes.py
+++ restaurantApp/routes.py
@@ -1,7 +1,7 @@
 import psycopg2 as pg
-from flask import Flask, render_template, redirect, flash,url_for
+from flask import Flask, render_template, redirect, flash,url_for, session
 from restaurantApp.forms import RegisterationForm, LoginForm, searchForm
-from restaurantApp import app
+from restaurantApp import app, bcrypt, login_manager
 
 dbname = "project"
 password = "Password@123"
@@ -11,7 +11,14 @@ cur = conn.cursor()
 @app.route('/')
 @app.route('/home')
 def home():
-   return render_template('home.html')
+    return render_template('home.html')
+
+@app.route('/getSession')
+def getSession():
+    if 'user' in session:
+        return str(session['user'])
+
+    return "NOT LOGGED IN"
 
 @app.route('/about')
 def about():
@@ -23,7 +30,7 @@ def search():
    restaurants= cur.fetchall()
    form= searchForm()
    if form.validate_on_submit():
-      query="SELECT distinct restaurant_name,address, cuisines, averagecost_for_two, currency, aggregate_rating from rest, city, cuisine,rest_cuisine WHERE rest.localityid=city.id and lower(city.city)=lower('"+str(form.city.data)+"') "
+      query="SELECT distinct restaurant_name,address, cuisines, averagecost_for_two, currency, aggregate_rating, restaurant_id from rest, city, cuisine,rest_cuisine WHERE rest.localityid=city.id and lower(city.city)=lower('"+str(form.city.data)+"') "
       if(form.restaurantName.data!=""):
          query+=" and lower(rest.restaurant_name) like lower('%" + form.restaurantName.data + "%') "
       if(form.locality.data!=""):
@@ -46,41 +53,56 @@ def search():
    
 @app.route('/userRegister', methods=['GET','POST'])
 def userRegister():
-   form= RegisterationForm()
-   if form.validate_on_submit():
-      date="2018-12-30"
-      gender="M"
-      address="153 Phulkian Enclave, New Delhi"
-      locality="Hauz Khas"
-      name= form.name.data
-      email=form.email.data
-      password=form.password.data
-      cur.execute("Select email from users where email= '"+email+"';")
-      Email=cur.fetchall()
-      if(Email==[]):
-         flash(f'Account created for {form.name.data}!', 'success')
-         cur.execute("INSERT into users (password, name, email, dob, gender, address, locality) values " \
-                     "(%s, %s, %s, %s, %s,%s,%s);", (password,name, email, date, gender, address, locality))
-         conn.commit()
-         return redirect(url_for('home'))
-      else:
-         flash('Email already exists!!!!', 'danger')
-      
-   return render_template('userRegister.html',title='User Register', form=form)
+    form= RegisterationForm()
+    if form.validate_on_submit():
+        hashedPassword= bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        date="2018-12-30"
+        gender="M"
+        address="153 Phulkian Enclave, New Delhi"
+        locality="Hauz Khas"
+        name= form.name.data
+        email=form.email.data
+        cur.execute("Select email from users where email= '"+email+"';")
+        Email=cur.fetchall()
+        if(Email==[]):
+            flash(f'Account created for {form.name.data}!', 'success')
+            cur.execute("INSERT into users (password, name, email, dob, gender, address, locality) values " \
+                        "(%s, %s, %s, %s, %s,%s,%s);", (hashedPassword,name, email, date, gender, address, locality))
+            conn.commit()
+            return redirect(url_for('home'))
+        else:
+            flash('Email already exists!!!!', 'danger')
+        
+    return render_template('userRegister.html',title='User Register', form=form)
 
 @app.route('/userLogin', methods=['GET','POST'])
 def userLogin():
-   form= LoginForm()
-   if form.validate_on_submit():
-      cur.execute("select password from users where email='"+str(form.email.data)+"';")
-      password=cur.fetchall()
-      if([]==password):
-         flash('Email not found. Please check your email', 'danger')
-      else:
-         password=password[0][0]
-         if form.password.data == password:
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
-         else:
-            flash('Login Unsuccessful. Please check email and password', 'danger')
-   return render_template('userlogin.html',title='User Login', form=form)
+    form= LoginForm()
+    if form.validate_on_submit():
+        cur.execute("select password, user_id from users where email='"+str(form.email.data)+"';")
+        userDetails=cur.fetchall()
+        if([]==userDetails):
+            flash('Email not found. Please check your email', 'danger')
+        else:
+            hashedPassword=userDetails[0][0]
+            if bcrypt.check_password_hash(hashedPassword,form.password.data):
+                flash('You have been logged in!', 'success')
+                session['user']=userDetails[0][1]
+                return redirect(url_for('home'))
+            else:
+                flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('userlogin.html',title='User Login', form=form)
+
+@app.route('/userLogout')
+def userLogout():
+    session.pop('user', None)
+    flash('You have been logged out!', 'success')
+    return redirect(url_for('home'))
+
+@app.route('/find/<ID>', methods=['GET','POST'])
+def findRestaurant(ID):
+    query="SELECT restaurant_name,address, cuisines, averagecost_for_two, currency, aggregate_rating FROM rest WHERE restaurant_id= '"+str(ID)+"';"
+    cur.execute(query)
+    restaurant= cur.fetchall()
+    return render_template('restaurant.html',title=restaurant[0][0], restaurant=restaurant[0])
+   
