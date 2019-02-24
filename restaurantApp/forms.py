c6ea519db1c50ccdd8595869077f993bfc3389b6
Vishesh Goyal <vsg2608@gmail.com>
2019-02-24 23:06:11 +0530

    Transactions working

--- restaurantApp/forms.py
+++ restaurantApp/forms.py
@@ -1,15 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, DateField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, RadioField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.fields.html5 import DateField, DateTimeField
 
 class RegisterationForm(FlaskForm):
     name        = StringField('Name',
                     validators=[DataRequired(),Length(min=2, max=20)])
     email       = StringField('Email',validators=[DataRequired(),Email()])
     password    = PasswordField('Password',validators=[DataRequired()])
-    date        = DateField("Date",format="%Y-%M-%d", validators=[DataRequired()])
+    date        = DateField("Date of Birth",format="%Y-%m-%d", validators=[DataRequired()])
+    gender      = RadioField("Gender", choices=[('M','Male'),('F','Female')])
+    address     = TextAreaField("Address", validators=[DataRequired()])
+    locality    = StringField("Locality", validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit      = SubmitField('Sign Up')
+
+class UpdateForm(FlaskForm):
+    name        = StringField('Name',
+                    validators=[DataRequired(),Length(min=2, max=20)])
+    email       = StringField('Email',validators=[DataRequired(),Email()])
+    date        = DateField("Date",format="%Y-%M-%d", validators=[DataRequired()])
+    submit      = SubmitField('Update')
 
 class LoginForm(FlaskForm):
     email       = StringField('Email',validators=[DataRequired(),Email()])
@@ -17,9 +28,11 @@ class LoginForm(FlaskForm):
     remember    = BooleanField('Remember Me')
     submit      = SubmitField('Log In')
 
+######################################################################################################
+
 class searchForm(FlaskForm):
     city        = StringField('City',
-                    validators=[DataRequired(),Length(min=2, max=20)])
+                    validators=[DataRequired()])
     restaurantName= StringField('Restaurant Name')
     locality    = StringField('Locality')
     rating      = IntegerField('Rating')
@@ -27,3 +40,15 @@ class searchForm(FlaskForm):
     #cuisine   = StringField()
     #cusinineOR    = StringField()
     #Cost range     
+
+class homeSearch(FlaskForm):
+    city        = StringField('City:', validators=[DataRequired()])
+    submit      = SubmitField('Find Restaurants')
+
+ ####################################################################################
+
+class tableBookingForm(FlaskForm):
+    name            = StringField('Name', validators=[DataRequired()])
+    noOfMembers     = IntegerField('Table for',validators=[DataRequired()])
+    date        = DateTimeField("Booking Date, Time",format="%Y-%m-%d", validators=[DataRequired()])
+    submit          = SubmitField('Book Table')
--- restaurantApp/routes.py
+++ restaurantApp/routes.py
@@ -1,8 +1,8 @@
 import psycopg2 as pg
 from flask import Flask, render_template, redirect, flash,url_for, session
-from restaurantApp.forms import RegisterationForm, LoginForm, searchForm
+from restaurantApp.forms import RegisterationForm, LoginForm, searchForm, UpdateForm, homeSearch, tableBookingForm
 from restaurantApp import app, bcrypt, login_manager
-
+import datetime
 dbname = "project"
 password = "Password@123"
 conn = pg.connect(database = dbname, user = "postgres", password = password, host = "13.233.41.140", port = "5432")
@@ -11,57 +11,28 @@ cur = conn.cursor()
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
-
-@app.route('/getSession')
-def getSession():
-    if 'user' in session:
-        return str(session['user'])
-
-    return "NOT LOGGED IN"
+    form = homeSearch()
+    return render_template('home.html', form=form)
 
 @app.route('/about')
 def about():
-   return render_template('about.html',title='About')
+    image_file= url_for('static', filename='profile_pics/default.jpg')
+    return render_template('about.html',title='About', image_file=image_file)
 
-@app.route('/search', methods=['GET','POST'])
-def search():
-   cur.execute("""SELECT restaurant_name,address, cuisines, averagecost_for_two, currency, aggregate_rating FROM rest LIMIT 10;""")
-   restaurants= cur.fetchall()
-   form= searchForm()
-   if form.validate_on_submit():
-      query="SELECT distinct restaurant_name,address, cuisines, averagecost_for_two, currency, aggregate_rating, restaurant_id from rest, city, cuisine,rest_cuisine WHERE rest.localityid=city.id and lower(city.city)=lower('"+str(form.city.data)+"') "
-      if(form.restaurantName.data!=""):
-         query+=" and lower(rest.restaurant_name) like lower('%" + form.restaurantName.data + "%') "
-      if(form.locality.data!=""):
-         query+=" and lower(city.locality) like lower('%" + form.locality.data + "%') "
-      if(form.rating.data!=""):
-         query+=" and rest.aggregate_rating>= "+str(form.rating.data)
-      cuisines="'japanese','korean'"
-      and_or=False
-      if(cuisines!=""):
-        query+= " and rest.restaurant_id= rest_cuisine.rest_id and cuisine.cuisine_code= rest_cuisine.cuisine_code and lower(cuisine.cuisine) in ("+cuisines+") "
-
-      sortbyRating= False
-      sortbyRestName= False
-      sortbyPrice= True
-      if(sortbyPrice):
-         query+= "ORDER BY rest.aggregate_rating desc"
-      cur.execute(query+";")
-      restaurants= cur.fetchall()
-   return render_template('search.html',title='Find Restaurant',restaurants=restaurants, form=form)
-   
+######################################### Login Logout routes ###############################################
 @app.route('/userRegister', methods=['GET','POST'])
 def userRegister():
     form= RegisterationForm()
     if form.validate_on_submit():
         hashedPassword= bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        date="2018-12-30"
-        gender="M"
-        address="153 Phulkian Enclave, New Delhi"
-        locality="Hauz Khas"
+        
+        date= form.date.data    
+        gender= form.gender.data   
+        address=form.address.data
+        locality= form.locality.data
         name= form.name.data
         email=form.email.data
+        print(name, email, gender, date, address, locality)
         cur.execute("Select email from users where email= '"+email+"';")
         Email=cur.fetchall()
         if(Email==[]):
@@ -99,10 +70,70 @@ def userLogout():
     flash('You have been logged out!', 'success')
     return redirect(url_for('home'))
 
+@app.route('/userAccount')
+def userAccount():
+    if('user' in session):
+        form=UpdateForm()
+        cur.execute("SELECT name, email FROM users WHERE user_id='"+str(session['user'])+"';")
+        user= cur.fetchall()[0]
+        image_file= url_for('static', filename='profile_pics/default.jpg')
+        return render_template('userAccount.html',title='User Account', user=user, image_file=image_file, form=form)
+    else:
+        flash('Please login to acess this account', 'danger')
+        return redirect(url_for('userLogin'))
+
+
+########################################### Restaurant routes #####################################
 @app.route('/find/<ID>', methods=['GET','POST'])
 def findRestaurant(ID):
     query="SELECT restaurant_name,address, cuisines, averagecost_for_two, currency, aggregate_rating FROM rest WHERE restaurant_id= '"+str(ID)+"';"
     cur.execute(query)
     restaurant= cur.fetchall()
-    return render_template('restaurant.html',title=restaurant[0][0], restaurant=restaurant[0])
+    image_file= url_for('static', filename='restaurant_pics/default.jpg')
+    form= tableBookingForm()
+    
+    if form.validate_on_submit():
+        if('user' in session):
+            userid= session['user']
+            restid= ID
+            bookedon= datetime.datetime.now()
+            bookedfor= form.date.data
+            no_people= form.noOfMembers.data
+            cur.execute("INSERT into transaction (userid, restid, bookedon, bookedfor, no_people ) values " \
+                        "(%s, %s, %s, %s, %s);", (userid, restid, bookedon, bookedfor, no_people))
+            conn.commit()
+        else:
+            flash('Please login to book the table', 'danger')
+        return redirect(url_for('userLogin'))
+    return render_template('restaurant.html',title=restaurant[0][0], restaurant=restaurant[0], image_file=image_file, form=form)
+
+
+@app.route('/search', methods=['GET','POST'])
+def search():
+   cur.execute("""SELECT restaurant_name,address, cuisines, averagecost_for_two, currency, aggregate_rating, restaurant_id FROM rest LIMIT 10;""")
+   restaurants= cur.fetchall()
+   form= searchForm()
+   if form.validate_on_submit():
+        print(form.rating.data==None)
+        query="SELECT distinct restaurant_name,address, cuisines, averagecost_for_two, currency, aggregate_rating, restaurant_id from rest, city, cuisine,rest_cuisine WHERE rest.localityid=city.id and lower(city.city)=lower('"+str(form.city.data)+"') "
+        if(form.restaurantName.data!=""):
+            query+=" and lower(rest.restaurant_name) like lower('%" + form.restaurantName.data + "%') "
+        if(form.locality.data!=""):
+            query+=" and lower(city.locality) like lower('%" + form.locality.data + "%') "
+        if(form.rating.data!="" and form.rating.data!=None):
+            query+=" and rest.aggregate_rating>= "+str(form.rating.data)
+        cuisines="'japanese','korean'"
+        and_or=False
+        if(cuisines!=""):
+            query+= " and rest.restaurant_id= rest_cuisine.rest_id and cuisine.cuisine_code= rest_cuisine.cuisine_code and lower(cuisine.cuisine) in ("+cuisines+") "
+
+        sortbyRating= False
+        sortbyRestName= False
+        sortbyPrice= True
+        if(sortbyPrice):
+            query+= "ORDER BY rest.aggregate_rating desc"
+        print(query)
+        cur.execute(query+";")
+        restaurants= cur.fetchall()
+   return render_template('search.html',title='Find Restaurant',restaurants=restaurants, form=form)
    
